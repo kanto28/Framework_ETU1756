b5386955613c683b251624918e084cc56fd52a52
kanto28 <kantoandrianaivosoa28@gmail.com>
2023-07-03 22:56:05 +0100

    Sprint9
    Sprint9

---
+++ sprint9/FileUpload.java
@@ -0,0 +1,26 @@
+package framework;
+public class FileUpload{
+    String filename;
+    String path;
+    byte[] bytes;
+    public String getFilename() {
+        return filename;
+    }
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+    public String getPath() {
+        return path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+    public byte[] getBytes() {
+        return bytes;
+    }
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    
+}
